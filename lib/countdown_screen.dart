16a706a0383707d80deeef4be36728376ae427e9
CLS11 <crystall.101600@gmail.com>
2024-10-06 01:20:48 +0530

    Wrapped the countdown screen with time lapse.

--- lib/countdown_screen.dart
+++ lib/countdown_screen.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:new_year_app/countdown_page.dart';
 import 'package:new_year_app/landscape.dart';
 import 'package:new_year_app/phases.dart';
+import 'package:new_year_app/time_lapse.dart';
 
 class NewYearsCountdownScreen extends StatelessWidget {
-  const NewYearsCountdownScreen({super.key});
+  NewYearsCountdownScreen(
+      {required this.overrideStartDateTime, required this.doTick, super.key});
 
+  
+  final DateTime overrideStartDateTime;
+  final bool doTick;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Landscape(
-        mode: EnvironmentMode.night,
-        time: '8:40 PM',
-        year: '2021',
+    final now = DateTime.now();
+    return Scaffold(
+      body: TimeLapse(
+        overrideStartDateTime: overrideStartDateTime,
+        doTick: doTick,
+        dateTimeBuilder: (DateTime currentTime) {
+          return NewYearCountdownPage(
+            currentTime: currentTime,
+          );
+        },
       ),
     );
   }
