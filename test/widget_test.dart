32672824ed9f151459cf42e587f9e4f3c7114481
CLS11 <crystall.101600@gmail.com>
2024-10-05 23:50:57 +0530

    Widget testing.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,6 +10,8 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:new_year_app/main.dart';
 
+
+
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
