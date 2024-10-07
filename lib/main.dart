7c17b662aaf5bb5d940ab37bf7b8270aea8c8e4c
CLS11 <crystall.101600@gmail.com>
2024-10-07 12:32:36 +0530

    Changed the doTick.

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,8 @@ class NewYearApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: NewYearsCountdownScreen(
-        overrideStartDateTime: DateTime.parse('2024-12-31 23:59:51'),
-        doTick: false,
+        overrideStartDateTime: DateTime.parse('2024-12-31 23:59:55'),
+        doTick: true,
       ),
       debugShowCheckedModeBanner: false,
     );
