8b04c74af91661be926108d51bc32587cb5139f1
CLS11 <crystall.101600@gmail.com>
2024-10-07 12:34:00 +0530

    Countdown counter with animation.

--- lib/countdown_text.dart
+++ lib/countdown_text.dart
@@ -9,10 +9,10 @@ class CountdownText extends StatefulWidget {
 
 class _CountdownTextState extends State<CountdownText>
     with SingleTickerProviderStateMixin {
- late  AnimationController _showNumberController;
-  Interval _opacity = Interval(0.0, 0.4);
-  Interval _scale = Interval(0.0, 0.5, curve: Curves.elasticOut);
- late  int _displayNumber;
+  late AnimationController _showNumberController;
+  final Interval _opacity = const Interval(0.0, 0.4);
+  final Interval _scale = const Interval(0.0, 0.5, curve: Curves.elasticOut);
+  late int _displayNumber;
 
   @override
   void initState() {
@@ -20,7 +20,7 @@ class _CountdownTextState extends State<CountdownText>
 
     _showNumberController = AnimationController(
       vsync: this,
-      duration: Duration(
+      duration: const Duration(
         milliseconds: 1000,
       ),
     )..addListener(() {
@@ -32,10 +32,6 @@ class _CountdownTextState extends State<CountdownText>
       _showNumberController.forward();
     }
   }
-
-  bool _isCountingDown() =>
-      widget.secondsToNewYear == null &&
-      (widget.secondsToNewYear <= 9 && widget.secondsToNewYear > 0);
 
   @override
   void didUpdateWidget(CountdownText oldWidget) {
@@ -55,10 +51,14 @@ class _CountdownTextState extends State<CountdownText>
     _showNumberController.dispose();
   }
 
+  bool _isCountingDown() =>
+      widget.secondsToNewYear == null &&
+      (widget.secondsToNewYear <= 9 && widget.secondsToNewYear > 0);
+
   @override
   Widget build(BuildContext context) {
     if (!_isCountingDown()) {
-      return SizedBox();
+      return const SizedBox();
     }
     return Align(
       alignment: const Alignment(0.0, -0.3),
